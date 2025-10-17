322399b3ef49059baafbe7ed1d34701386dde4eb
yogireddy02 <yogavardhanreddy02@gmail.com>
2025-10-17 20:21:49 +0530

    server port line changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
