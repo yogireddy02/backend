2082cc4426775b94475e1475fa620937833dd792
yogireddy02 <yogavardhanreddy02@gmail.com>
2025-10-17 19:31:20 +0530

    package.json changes

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use('/api/blogs', blogRoutes);
 
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
